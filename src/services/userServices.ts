c4c6e3919ba04fe366bf18e5f4faa43b8575c0e8
william nascimento <williamnnmiranda@gmail.com>
2023-04-06 13:45:57 -0300

    feat: :sparkles: created user services

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,19 +1,15 @@
-/*
-  import { api } from "../helpers/api";
+import { api } from '../helpers/api';
 const userServices = {
-	login: async (data: any) => {
-		return await (
-			await api.post("/access/login", data)
-		).data;
-	},
-	auth: async (token: string) => {
-		return await (
-			await api.post("/access/auth", { token: token })
-		).data;
-	},
+  login: async (data: any) => {
+    return await (
+      await api.post('/access/login', data)
+    ).data;
+  },
+  auth: async (token: string) => {
+    return await (
+      await api.post('/access/auth', { token: token })
+    ).data;
+  },
 };
 
 export default userServices;
-
-
-*/
