e5b48f31d5d6f3174c098d20a5cb7d2ff35cf019
william nascimento <williamnnmiranda@gmail.com>
2023-04-10 16:13:32 -0300

    feat: :sparkles: created stacks router

--- src/stacks/index.tsx
+++ src/stacks/index.tsx
@@ -2,12 +2,13 @@ import { Text } from "react-native"
 import Auth from "./auth"
 import { UserContext } from "../contexts/UserContext"
 import { useContext } from "react"
+import Routes from "./routes"
 
 const Navigation = () => {
   const { authenticate } = useContext(UserContext)
   return (
     <>
-      {authenticate ? <Text> teste </Text> : <Auth />}
+      {authenticate ? <Routes /> : <Auth />}
     </>
   )
 }
